f890051c1574d9b3a577419a736af4b6aa8c2df1
viniciusdiasn <viniciusdiasbrnv@gmail.com>
2024-08-03 16:47:36 -0300

    feat script file

---
+++ exemplo.js
@@ -0,0 +1,23 @@
+var a
+let b
+const c = 5
+
+a='5'
+
+b='10'
+
+// comentarios
+function SomarNumeros(num1, num2) {
+    return num1 + num2
+}
+        
+console.log(SomarNumeros())
+console.log(a)
+console.log(b)
+console.log(c)
+
+function QuantidadedeCaracter(palavra) {
+    return palavra.length
+}
+
+console.log(QuantidadedeCaracter('palavra'))
